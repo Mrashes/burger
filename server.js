07f3b8f0a62c50b3e3a487fe0499942fb32f4808
unknown <mrashes2@gmail.com>
2017-08-15 18:14:59 -0500

    heroku working?

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require("express");
 var bodyParser = require("body-parser");
 var methodOverride = require("method-override");
 
-app.set('port', (process.env.PORT));
+app.set('port', (process.env.PORT || 3000));
 
 var app = express();
 
