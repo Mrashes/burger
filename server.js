0f70dbb664e0cdbeb0447e7c878bef95f3753b99
unknown <mrashes2@gmail.com>
2017-08-15 18:11:07 -0500

    working on heroku

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require("express");
 var bodyParser = require("body-parser");
 var methodOverride = require("method-override");
 
-app.set('port', (process.env.PORT || 3000));
+app.set('port', (process.env.PORT));
 
 var app = express();
 
