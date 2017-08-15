7d27214adf739362e80a084b41ab3410a8f736af
unknown <mrashes2@gmail.com>
2017-08-15 18:05:20 -0500

    fixing on heroku

--- config/connection.js
+++ config/connection.js
@@ -8,7 +8,7 @@ if (process.env.JAWSDB_URL) {
         host: "localhost",
         port: 3306,
         user: "root",
-        password: "",
+        password: "Shaina2013Walker",
         database: "burgers_db"
     });
 }
--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require("express");
 var bodyParser = require("body-parser");
 var methodOverride = require("method-override");
 
-var port = process.env.PORT || 3000;
+app.set('port', (process.env.PORT || 3000));
 
 var app = express();
 
@@ -25,4 +25,6 @@ var routes = require("./controllers/burgers_controller.js");
 
 app.use("/", routes);
 
-app.listen(port);
+app.listen(app.get('port'), function() {
+    console.log("App listening on PORT " + app.get('port'));
+  });
