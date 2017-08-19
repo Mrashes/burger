6635ebc14654d152ed4b4883bf3ac6856e4e2f98
unknown <mrashes2@gmail.com>
2017-08-19 09:36:00 -0500

    changed app order

--- server.js
+++ server.js
@@ -2,9 +2,9 @@ var express = require("express");
 var bodyParser = require("body-parser");
 var methodOverride = require("method-override");
 
-app.set('port', (process.env.PORT || 3000));
-
 var app = express();
+
+app.set('port', (process.env.PORT || 3000));
 
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static("public"));
