2427478eb9ba51aa46c96af3eb3d4c57e389f6b4
unknown <mrashes2@gmail.com>
2017-08-19 09:44:59 -0500

    changed connection details

--- config/connection.js
+++ config/connection.js
@@ -5,10 +5,10 @@ if (process.env.JAWSDB_URL) {
     connection = mysql.createConnection(process.eventNames.JAWSDB_URL);
 } else {
     connection = mysql.createConnection({
-        host: 	"cdm1s48crk8itlnr.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+        host: 	"gk90usy5ik2otcvi.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
         port: 3306,
-        user: "q83axpkpdwj7g2ei",
-        password: "fve72qedo7p9vedg",
+        user: "xcze2zf9hlnbvj8t",
+        password: "kusthcbyxz5e17ez",
         database: "burgers_db"
     });
 }
